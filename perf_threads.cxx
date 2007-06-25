b9c2a183606e1e84cbb24c5012a1bd97617aa8c5
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-06-25 16:17:53 +0200

    tweak the thread prg and add a big image to process
    darcs-hash:20070625141753-2fc9d-f8708313622b75bcbe7f4f5db7666aa788204ca2.gz

--- perf_threads.cxx
+++ perf_threads.cxx
@@ -12,7 +12,7 @@ int main(int, char * argv[])
   const int dim = 2;
   typedef unsigned char PType;
   typedef itk::Image< PType, dim > IType;
-  typedef unsigned char AType;
+  typedef float AType;
   typedef itk::Image< AType, dim > AccType;
 
   
@@ -21,18 +21,27 @@ int main(int, char * argv[])
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
   
-  typedef itk::BoxMeanImageFilter< IType, AccType > BoxConvType;
+  typedef itk::BoxMeanImageFilter< IType, IType > BoxConvType;
   BoxConvType::Pointer boxConv = BoxConvType::New();
   boxConv->SetInput( reader->GetOutput() );
-  boxConv->SetRadius( 5 );
+  boxConv->SetRadius( 2 );
   
-  typedef itk::MeanImageFilter< IType, AccType > DirectConvType;
+  typedef itk::MeanImageFilter< IType, IType > DirectConvType;
   DirectConvType::Pointer directConv = DirectConvType::New();
   directConv->SetInput( reader->GetOutput() );
   DirectConvType::InputSizeType radius;
-  radius.Fill( 5 );
+  radius.Fill( 2 );
   directConv->SetRadius( radius );
 
+  typedef itk::BoxAccumulatorImageFilter< IType, AccType > BoxAccType;
+  BoxAccType::Pointer acc = BoxAccType::New();
+  acc->SetInput( reader->GetOutput() );
+  
+  typedef itk::BoxMeanCalculatorImageFilter< AccType, IType > BoxMeanType;
+  BoxMeanType::Pointer mean = BoxMeanType::New();
+  mean->SetInput( acc->GetOutput() );
+  mean->SetRadius( radius );
+  
   
 /*  // write 
   typedef itk::ImageFileWriter< IType > WriterType;
@@ -45,32 +54,49 @@ int main(int, char * argv[])
   std::cout << "#nb" << "\t" 
             << "Direct" << "\t"
             << "Box" << "\t"
+            << "Acc" << "\t"
             << std::endl;
 
   for( int t=1; t<=10; t++ )
     {
     itk::TimeProbe origtime;
     itk::TimeProbe boxtime;
+    itk::TimeProbe acctime;
+    itk::TimeProbe meantime;
 
     boxConv->SetNumberOfThreads( t );
     directConv->SetNumberOfThreads( t );
+    acc->SetNumberOfThreads( t );
+    mean->SetNumberOfThreads( t );
     
-    for( int i=0; i<50; i++ )
+    for( int i=0; i<10; i++ )
       {
+      boxConv->Modified();
       boxtime.Start();
       boxConv->Update();
       boxtime.Stop();
-      boxConv->Modified();
 
+      directConv->Modified();
       origtime.Start();
       directConv->Update();
       origtime.Stop();
-      directConv->Modified();
+
+      acc->Modified();
+      acctime.Start();
+      acc->Update();
+      acctime.Stop();
+
+      mean->Modified();
+      meantime.Start();
+      mean->Update();
+      meantime.Stop();
       }
       
     std::cout << std::setprecision(3) << t << "\t" 
               << origtime.GetMeanTime() << "\t"
               << boxtime.GetMeanTime() << "\t"
+              << acctime.GetMeanTime() << "\t"
+              << meantime.GetMeanTime() << "\t"
               << std::endl;
     }
   
