c393d8a704f7477b4325f02a16d728b29ce97cff
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-06-25 16:55:20 +0200

    add perf3D
    darcs-hash:20070625145520-2fc9d-5733b1a9e22e0cd9061217d8b2377dd9a920d9da.gz

---
+++ perf3D.cxx
@@ -0,0 +1,101 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCommand.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkBoxMeanImageFilter.h"
+#include "itkMeanImageFilter.h"
+#include "itkTimeProbe.h"
+
+
+int main(int argc, char * argv[])
+{
+
+  if( argc !=  4)
+    {
+    std::cerr << "usage: " << argv[0] << " " << std::endl;
+    std::cerr << "  : " << argc << std::endl;
+    exit(1);
+    }
+
+  const int dim = 3;
+
+  itk::MultiThreader::SetGlobalDefaultNumberOfThreads(1);  
+  
+  typedef unsigned char PType;
+  typedef itk::Image< PType, dim > IType;
+  typedef unsigned char AType;
+  typedef itk::Image< AType, dim > AccType;
+
+  typedef itk::ImageFileReader< IType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  reader->Update();
+
+  typedef itk::BoxMeanImageFilter< IType, AccType > BoxConvType;
+  BoxConvType::Pointer boxConv = BoxConvType::New();
+  boxConv->SetInput( reader->GetOutput() );
+  //itk::SimpleFilterWatcher watcher(filter, "filter");
+  typedef itk::MeanImageFilter< IType, AccType > DirectConvType;
+  DirectConvType::Pointer directConv = DirectConvType::New();
+  directConv->SetInput( reader->GetOutput() );
+  DirectConvType::InputSizeType radius;
+
+  std::cout << "Size" << "\t"
+            << "Direct" << "\t"
+            << "Box" << "\t"
+            << std::endl;
+
+  std::vector<int> Sizes;
+
+  Sizes.push_back(1);
+  Sizes.push_back(3);
+  Sizes.push_back(10);
+
+  for (unsigned k = 0; k<Sizes.size();k++)
+    {
+    unsigned R = Sizes[k];
+
+    boxConv->SetRadius(R);
+    radius.Fill(R);
+    directConv->SetRadius(radius);
+    
+    itk::TimeProbe origtime;
+    itk::TimeProbe boxtime;
+
+    for( int i=0; i<2; i++ )
+      {
+      boxtime.Start();
+      boxConv->Update();
+      boxtime.Stop();
+      boxConv->Modified();
+
+      origtime.Start();
+      directConv->Update();
+      origtime.Stop();
+      directConv->Modified();
+
+      }
+    std::cout << R << "\t"
+              << origtime.GetMeanTime() << "\t"
+              << boxtime.GetMeanTime() << "\t"
+              << std::endl;
+
+    }
+
+  typedef itk::ImageFileWriter< AccType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( boxConv->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->Update();
+
+
+  writer->SetInput( directConv->GetOutput() );
+  writer->SetFileName( argv[3] );
+  writer->Update();
+  
+  
+
+  return 0;
+}
+
